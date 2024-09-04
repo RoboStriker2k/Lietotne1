7393ff744c1e0b618b90988c0e67b9bc7ef3c163
RoboStriker2k <rlaimite@gmail.com>
2024-09-04 14:55:58 +0300

    vairāku failu pievienošana jaunam ierakstam

--- source/components/upload.vue
+++ source/components/upload.vue
@@ -4,14 +4,39 @@
       <form v-on:submit.prevent="uploadFile">
         <h1>Pievienot</h1>
         <p>Ieraksta virsraksts</p>
-        <input type="text" v-model="title" placeholder="Ieraksta virsraksts" />
+        <input
+          type="text"
+          v-model="uploadstate.title"
+          placeholder="Ieraksta virsraksts"
+          @change="uploadstate.title = $event.target.value"
+        />
         <p>Ieraksta apraksts</p>
-        <input type="text" v-model="pdesc" placeholder="Ieraksta apraksts" />
+        <input
+          type="text"
+          v-model="uploadstate.pdesc"
+          placeholder="Ieraksta apraksts"
+          @change="uploadstate.pdesc = $event.target.value"
+        />
         <div>
           <p>Ieraksta attels (var nepievienot)</p>
           <img id="preview" />
-          <input id="fileupl" type="file" @change="onFileChange" />
+          <input id="fileupl" type="file" multiple @change="onMultipleFilesSelected" />
         </div>
+        <div
+          class="editgrid"
+          v-if="uploadstate.preview.length > 0"
+          v-for="item in uploadstate.preview"
+        >
+          <img id="preview" :src="item" />
+          <button
+            class="removebtn"
+            type="button"
+            @click="removefromupload(uploadstate.preview.indexOf(item))"
+          >
+            X
+          </button>
+        </div>
+
         <div>
           <button>Upload</button>
           <button @click="closebtn">Aizvert</button>
@@ -20,41 +45,81 @@
     </div>
   </div>
 </template>
-<script setup></script>
 
 <script>
 export default {
   name: 'upload',
 
   data() {
-    return {
+    const uploadstate = {
       title: '',
       pdesc: '',
-      file: null
+      files: [],
+      preview: []
+    }
+    return {
+      uploadstate
     }
   },
   methods: {
-    onFileChange(event) {
-      this.file = event.target.files[0]
+    onMultipleFilesSelected(event) {
+      let filearr = []
+      for (let i = 0; i < event.target.files.length; i++) {
+        filearr.push(event.target.files[i])
+        this.renderpic(event.target.files[i])
+      }
+      this.uploadstate = { ...this.uploadstate, files: filearr, statenr: 1 }
+    },
 
+    renderpic(img) {
+      let imgarr = this.uploadstate.preview
       const reader = new FileReader()
       reader.onload = (e) => {
-        let img = document.getElementById('preview')
-        img.src = e.target.result
+        if (e.target) {
+          imgarr.push(e.target.result) ?? ''
+        }
       }
-      reader.readAsDataURL(this.file)
+      reader.readAsDataURL(img)
+      this.uploadstate = { ...this.uploadstate, preview: imgarr, statenr: 1 }
     },
+    removefromupload(fileindex) {
+      let pics = this.uploadstate.preview
+      let filearr = this.uploadstate.files
+      if (fileindex != -1) {
+        pics.splice(fileindex, 1)
+      }
+      if (uploadstate.files != null) {
+        if (fileindex != -1) {
+          filearr.splice(fileindex, 1)
+        }
+      }
+      this.uploadstate = { ...this.uploadstate, files: filearr, preview: pics, statenr: 1 }
+    },
+
     uploadFile() {
       const formData = new FormData()
-      formData.append('title', this.title)
-      formData.append('pdesc', this.pdesc)
-      formData.append('file', this.file)
+      formData.append('title', this.uploadstate.title)
+      formData.append('pdesc', this.uploadstate.pdesc)
+      if (this.uploadstate.files != null) {
+        for (let i = 0; i < this.uploadstate.files.length; i++) {
+          formData.append('file', this.uploadstate.files[i])
+        }
+      }
       fetch('http://localhost:3000/api/addpost', {
         method: 'post',
         body: formData
       }).catch(function (error) {
         console.log(error)
       })
+
+      this.uploadstate = {
+        ...this.uploadstate,
+        title: '',
+        pdesc: '',
+        files: [],
+        preview: [],
+        statenr: 0
+      }
 
       this.$emit('update')
     },
